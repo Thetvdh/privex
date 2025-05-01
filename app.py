824f5a763d8b4abcba6fefa1f4863eb17d9243e1
Thetvdh <tvdjthe@protonmail.com>
2025-05-01 11:17:40 +0100

    Created search page with search functionality built

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-
+import re
 import datetime
+import secrets
 
 app = Flask(__name__)
-
+app.secret_key = secrets.token_hex(64)
 @app.route('/')
 
 def home():  # put application's code here
+
+    # temporary session setter
+    session["username"] = "admin"
+    session["is_admin"] = True # change when testing different features
     return redirect(url_for('search'))
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -15,7 +20,22 @@ def login():
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
-    return render_template("search.html")
+
+    test_data = {
+        "WINSERVFYP.FYP.LOC": "1",
+        "LINSERVFYP.FYP.LOC": "2",
+        "FYPDC.FYP.LOC": "3"
+    }
+
+    if request.method == "GET":
+        return render_template("search.html")
+    query = request.form.get("query")
+    r = re.compile(query, re.IGNORECASE)
+    search_results = [(key, value) for key, value in test_data.items() if r.search(key)]
+    if len(search_results) == 0:
+        search_results = False
+
+    return render_template("search.html", search_results=search_results)
 
 @app.route('/logout')
 def logout():
@@ -25,6 +45,7 @@ def logout():
 
 @app.route('/computer/<computer_id>', methods=['GET', 'POST'])
 def computer(computer_id):
+
 
     users = ["FYP\\Zythia",
 "FYP\\zythum",
@@ -47,11 +68,19 @@ def computer(computer_id):
             {
                 "username": "FYP\\demoadmin",
                 "start_time": datetime.datetime.now(),
+                "expiry_time": datetime.datetime.now() + datetime.timedelta(hours=4),
                 "reason": "Software upgrade"
             }
         ]
     }
     return render_template("computer_info.html", computer_details=mock_details)
+
+@app.route("/session", methods=['GET', 'POST'])
+def session_manager():
+    if request.method == "POST":
+        reason = request.json.get("reason")
+        print("Reason: ", reason)
+    return "<h1>GET</h1>"
 
 
 if __name__ == '__main__':
