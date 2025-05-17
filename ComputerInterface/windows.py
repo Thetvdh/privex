32ee81728e053fa21000e1564f26f0dd09e70ef7
Thetvdh <tvdjthe@protonmail.com>
2025-05-17 23:29:51 +0100

    Fixed windows administration

--- ComputerInterface/windows.py
+++ ComputerInterface/windows.py
@@ -1,7 +1,9 @@
+import requests
 import winrm
 import yaml
 import subprocess
 import logging
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -17,37 +19,88 @@ def load_config():
         ad_config = yaml.safe_load(config_file)
     return ad_config
 
-# TODO Fix, research SPNs
 class WindowsWorker:
     def __init__(self):
         self.AD_CONFIG = load_config()
         self.user_name = self.AD_CONFIG['WorkerUsername']
         self.password = self.AD_CONFIG["WorkerPassword"]
+        self.net_bios = self.AD_CONFIG["DomainNetBIOSName"]
+
+    # Gets a kerberos ticket for the worker account. Ticket is needed in order for WinRM to work as kerberos transport
+    # requires it. Needs to be generated with kinit hence use of subprocess
     def get_kerberos_ticket(self):
         try:
-            kinit_cmd = f"echo '{self.password}' | kinit {self.user_name}@FYP.LOC"
-            subprocess.run(kinit_cmd, shell=True, check=True, executable="/bin/bash")
+            klist_cmd = "klist"
+            output = subprocess.run(klist_cmd, shell=True, check=True, executable="/bin/bash", stdout=subprocess.DEVNULL)
+            if output.returncode == 0:
+                logging.info("Kerberos ticket already valid")
+                return True
+        except subprocess.CalledProcessError as e:
+            logging.error("Failed to check status of Kerberos Ticket")
+        try:
+            kinit_cmd = f"echo '{self.password}' | kinit {self.user_name}@{self.AD_CONFIG['DomainDNSName']}"
+            subprocess.run(kinit_cmd, shell=True, check=True, executable="/bin/bash", stdout=subprocess.DEVNULL)
             logging.info("Successfully got Kerberos Ticket")
         except subprocess.CalledProcessError as e:
-            logging.error(e)
+            logging.error("Failed to obtain Kerberos Ticket")
             return False
         return True
 
-    def establish_winrm_session(self, computer_fqdn):
-        self.get_kerberos_ticket()
-        user_with_principal = f"{self.user_name}@FYP.LOC"
-        return winrm.Session(computer_fqdn, auth=(user_with_principal, self.password), transport="kerberos", server_cert_validation="ignore")
 
+    # returns a WinRM session for the specified computer
+    def establish_winrm_session(self, computer_fqdn):
+        if self.get_kerberos_ticket():
+            user_with_principal = f"{self.user_name}@{self.AD_CONFIG['DomainDNSName']}" # @principal is required for transport to work correctly
+            return winrm.Session(computer_fqdn, auth=(user_with_principal, self.password), transport="kerberos", server_cert_validation="ignore")
+        else:
+            logging.error("Failed to get Kerberos ticket")
+            return False
+
+    # returns a list of admins in the format NETBIOS\\samAccountName
+    # It gets both domain users and locally added / created users
     def get_computer_administrators(self, session):
-        ps_script_get_administrators = """
-        Get-LocalGroupMember -Name "Administrators" | Where-Object { $_.ObjectClass -eq 'User' } | Select-Object -ExpandProperty Name
+        try:
+            ps_script_get_administrators = """
+            Get-LocalGroupMember -Name "Administrators" | Where-Object { $_.ObjectClass -eq 'User' } | Select-Object -ExpandProperty Name
+            """
+            response = session.run_ps(ps_script_get_administrators)
+            admins = response.std_out.decode()
+            admin_list = admins.split("\r\n")
+            clean_admin_list = []
+            for admin in admin_list:
+                admin = admin.strip()
+                if admin:
+                    clean_admin_list.append(admin)
+            return clean_admin_list
+        except requests.exceptions.ConnectTimeout as e:
+            logging.error(e)
+            return []
+        except requests.exceptions.ConnectionError as e:
+            logging.error(e)
+            return []
+    def add_windows_admin(self, session, username):
+        ps_script_add_administrator = f"""
+        Add-LocalGroupMember -Group "Administrators" -Member {username}
         """
-        response = session.run_ps(ps_script_get_administrators)
-        admins = response.std_out.decode()
-        admin_list = admins.split("\r\n")
-        clean_admin_list = []
-        for admin in admin_list:
-            admin = admin.strip()
-            if admin:
-                clean_admin_list.append(admin)
-        return clean_admin_list
+        response = session.run_ps(ps_script_add_administrator)
+        if response.status_code == 0:
+            logging.info(f"Successfully added {username} from Administrators group")
+        else:
+            logging.warning(f"Failed to add {username} from Administrators group, this could be as they were already present in the group")
+
+    def remove_windows_admin(self, session, username):
+        ps_script_add_administrator = f"""
+        Remove-LocalGroupMember -Group "Administrators" -Member {username}
+        """
+        response = session.run_ps(ps_script_add_administrator)
+        if response.status_code == 0:
+            logging.info(f"Successfully removed {username} from Administrators group")
+        else:
+            logging.warning(f"Failed to remove {username} from Administrators group, this could be because they did not exist in the group")
+
+    def check_admin_removed(self, session, username):
+        admins = self.get_computer_administrators(session)
+        print(f"Username: {username}\nAdmins: {admins}")
+        if username not in admins:
+            return True
+        return False
--- runner.py
+++ runner.py
@@ -1,8 +1,95 @@
 from Scanner import scanner
 from DB.DBController import DBInterface
+import yaml
+# admins = scanner.get_computer_admins_windows('WINSERVFYP.FYP.LOC')
+#
+# interface = DBInterface()
+#
+# interface.setup_add_computer_admins(admins, "WINSERVFYP.FYP.LOC", "FYP")
 
-admins = scanner.get_computer_admins_windows('WINSERVFYP.FYP.LOC')
+# def setup():
+#     # Initial setup for users and computers
+#     scanner.add_computers()
+#     scanner.add_users()
+#
+#     # Gets the admins already on the computer and adds them to the authorised admins list
+#     interface.setup_add_computer_admins(admins, "WINSERVFYP.FYP.LOC", "FYP")
 
-interface = DBInterface()
+def load_config():
+    with open("domain_config.yaml", "r") as config_file:
+        ad_config = yaml.safe_load(config_file)
+    return ad_config
 
-interface.setup_add_computer_admins(admins, "WINSERVFYP.FYP.LOC", "FYP")
+
+def main_loop():
+    """
+    Steps:
+    1) Get all AD Users
+    2) Get all AD Computers
+    3) Get Admins
+    4) Add and Remove as applicable
+    """
+    ad_config = load_config()
+
+    scanner.add_users() # Step 1
+    scanner.add_computers() # Step 2
+
+    # Step 3
+
+    # Get all computers
+    interface = DBInterface()
+    db_computers = interface.get_all_database_computers()
+    # Get admin list for each computer
+    for computer in db_computers:
+        # Get admin list from computer itself (windows)
+        if "Windows" in computer[1]:
+            admins_from_computer = scanner.get_computer_admins_windows(computer[0])
+        else:
+            # TODO Linux check
+            continue
+        if not admins_from_computer:
+            continue
+        # Get admin list from db
+        admins_from_db = interface.get_computer_admins(computer[0])
+        print(f"Computer: {computer}\nAdmin List (Windows): {admins_from_computer}\nAdmin List (DB): {admins_from_db}")
+
+        # Convert admin from windows to IDs
+        admins_from_computer_database_ids = []
+        for admin in admins_from_computer:
+            if admin[:3] == ad_config["DomainNetBIOSName"]:
+                tmp = admin.split("\\")[-1]
+                admin_user_id = interface.get_user_id(tmp)
+                admins_from_computer_database_ids.append(admin_user_id)
+            else:
+                # If the account is a local account then it will be added using its FQDN as it will not have a database ID
+                admins_from_computer_database_ids.append(admin)
+        print(f"Admin List ID (Database from Windows): {admins_from_computer_database_ids}")
+        # Compare lists
+        for user in admins_from_computer_database_ids:
+            # User should be in the admin list, no issue
+            if user in admins_from_db:
+                print(f"User {user} is a valid admin")
+            else:
+                print(f"User {user} is not a valid admin")
+                # remove invalid admin from target
+                user_name_from_id = interface.get_user_from_id(user)
+                if not user_name_from_id: # Likely means it is a local account and therefore the account name is the same as the ID
+                    user_name_from_id = user
+                scanner.remove_admin_windows(computer[0], user_name_from_id)
+                if scanner.check_admin_removed(computer[0], user_name_from_id):
+                    print(f"Admin {user} successfully removed")
+                else:
+                    print(f"Admin {user} unsuccessfully removed")
+
+# test
+
+if __name__ == '__main__':
+    # main_loop()
+    # sudoers = scanner.get_computer_admins_linux("LINSERVFYP.FYP.LOC")
+    # print(sudoers)
+    # result = scanner.add_sudoer_linux("LINSERVFYP.FYP.LOC", "basic@FYP.LOC")
+    # print(result)
+    # sudoers = scanner.get_computer_admins_linux("LINSERVFYP.FYP.LOC")
+    # print(sudoers)
+    result = scanner.remove_sudoer_linux("LINSERVFYP.FYP.LOC", "basic@FYP.LOC")
+    print(result)
