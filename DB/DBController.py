6edf5fd8388134c6198d4b83ada9ca9c2f25d110
Thetvdh <tvdjthe@protonmail.com>
2025-05-17 23:31:39 +0100

    updated DB interface functionality

--- DB/DBController.py
+++ DB/DBController.py
@@ -1,6 +1,7 @@
 import sqlite3
 import uuid
 import logging
+from distutils.command.clean import clean
 
 logging.basicConfig(
     level=logging.INFO,
@@ -10,7 +11,10 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%d/%m/%Y %I:%M:%S",
 )
-
+"""
+DBConnector class. Functions to connect to the database.
+Intended to be used as a parent class
+"""
 class DBConnector:
     def __init__(self):
         self.name = f"DB/privex.db"
@@ -18,6 +22,7 @@ class DBConnector:
         self.cursor = None
         self.establish_connection()
 
+    # Attempts SQLite database connection
     def establish_connection(self):
         try:
             self.connection = sqlite3.connect(self.name)
@@ -29,20 +34,27 @@ class DBConnector:
         if self.connection is not None:
             self.connection.close()
 
+"""
+DBInterface class
 
+All activities involving the database are contained within this class. This class should be the only class making
+requests to and from the SQLite Database
+"""
 class DBInterface(DBConnector):
     def __init__(self):
         super().__init__()
 
+    # Adds a single computer to the database
     def add_computer(self, computer: dict):
+        # UUIDs are generated and then added to the database as a unique identifier for each computer
         computer_id = uuid.uuid4().hex
         while self.check_unique_computer_id(computer_id):
-            computer_id = uuid.uuid4()
+            computer_id = uuid.uuid4().hex
         computer["computer_id"] = computer_id
         try:
             sql = """
             INSERT INTO computers (computer_id, computer_name, objectSid, ip_addr, operating_system) VALUES (?, ?, ?, ?, ?)
-            """
+            """ # SQL adds computer to the DB
             self.cursor.execute(sql, (computer["computer_id"], computer["FQDN"].upper(), computer["objectSid"], computer["ip_addr"], computer["operating_system"]))
             self.connection.commit()
             logging.info("Successfully added computer")
@@ -51,6 +63,7 @@ class DBInterface(DBConnector):
             logging.error(f"{error}")
             return False
 
+    # Function to add a single user to the database
     def add_user(self, user: dict):
         user_id = uuid.uuid4().hex
         while self.check_unique_user_id(user_id):
@@ -68,6 +81,19 @@ class DBInterface(DBConnector):
             logging.error(f"{error}")
             return False
 
+    def get_all_database_computers(self) -> list:
+        try:
+            sql = """
+            SELECT computer_name, operating_system FROM computers
+            """
+            self.cursor.execute(sql)
+            computers = self.cursor.fetchall()
+            if computers is None:
+                return []
+            return computers
+        except sqlite3.OperationalError as error:
+            logging.error(f"{error}")
+            return []
     # Checks if the UUID generated is already in the database, returns False if the computer ID is not in the database
     def check_unique_computer_id(self, computer_id):
         sql = """
@@ -79,6 +105,7 @@ class DBInterface(DBConnector):
             return False
         else:
             return True
+    # Checks if the UUID generated is already in the database, returns False if the user ID is not in the database
 
     def check_unique_user_id(self, user_id):
         sql = """
@@ -91,7 +118,7 @@ class DBInterface(DBConnector):
         else:
             return True
 
-    # Returns false if the computer is not already in the database
+    # Returns true if the computer is already in the database
     def check_unique_computer_sid(self, computer_sid):
         sql = """
         SELECT objectSid FROM computers WHERE objectSid = ?
@@ -103,6 +130,7 @@ class DBInterface(DBConnector):
         else:
             return True
 
+    # returns true if the SID is not unique
     def check_unique_user_sid(self, user_sid):
         sql = """
         SELECT objectSid FROM ad_users WHERE objectSid = ?
@@ -114,6 +142,7 @@ class DBInterface(DBConnector):
         else:
             return True
 
+    # requests the computer ID from the database based off the FQDN
     def get_computer_id(self, computer_name):
         sql = """
         SELECT computer_id FROM computers WHERE computer_name = ?
@@ -125,11 +154,23 @@ class DBInterface(DBConnector):
             return False
         return data[0]
 
+    # requests the user ID from the database based off the samAccountName
     def get_user_id(self, samAccountName):
         sql = """
         SELECT user_id FROM ad_users WHERE samAccountName = ?
         """
         self.cursor.execute(sql, [samAccountName])
+        data = self.cursor.fetchone()
+        if data is None:
+            logging.warning("Unable to find user")
+            return False
+        return data[0]
+
+    def get_user_from_id(self, user_id):
+        sql = """
+        SELECT samAccountName from ad_users WHERE user_id = ?
+        """
+        self.cursor.execute(sql, [user_id])
         data = self.cursor.fetchone()
         if data is None:
             logging.warning("Unable to find user")
@@ -146,6 +187,8 @@ class DBInterface(DBConnector):
         """
         for admin in admin_list:
             try:
+                # Check to see if the account is a domain account or a local account.
+                # It is important to distinct this as the domain administrator has the same name as all local administrators
                 if admin[:3] == domain_netbios:
                     tmp = admin.split("\\")[-1]
                     admin_user_id = self.get_user_id(tmp)
@@ -155,10 +198,29 @@ class DBInterface(DBConnector):
                     logging.info("Successfully added admin")
                 else:
                     domain_account = False
-                    admin_user_id = "LOCAL ACCOUNT"
+                    admin_user_id = admin # Test how this works with removal from admins
                     self.cursor.execute(sql, (computer_id, admin_user_id, persistent, domain_account))
                     self.connection.commit()
                     logging.info("Successfully added admin")
             except sqlite3.OperationalError as error:
                 logging.error(f"{error}")
                 return False
+
+
+    def get_computer_admins(self, computer_name) -> list:
+
+        sql_computer_id = """
+        SELECT computer_id FROM computers WHERE computer_name = ?
+        """
+        self.cursor.execute(sql_computer_id, [computer_name])
+        computer_id = self.cursor.fetchone()[0]
+
+        sql_admins = """
+        SELECT user_id FROM authorised_admins WHERE computer_id = ?
+        """
+        self.cursor.execute(sql_admins, [computer_id])
+        data = self.cursor.fetchall()
+        clean_list = []
+        for item in data:
+            clean_list.append(item[0])
+        return clean_list
